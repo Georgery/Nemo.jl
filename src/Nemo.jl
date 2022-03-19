3aa93063cd2422b716c0a04747431dcedd68a420
Georgery <forfuncies@gmx.de>
2022-03-19 17:18:44 +0100

    Request Checks added
    The request is now checked to have the right fields in the right
    formats. Furthermore, numbers must be delivered as numbers and not as
    strings anymore. Oh, and documentation was added.

--- src/Nemo.jl
+++ src/Nemo.jl
@@ -39,13 +39,47 @@ struct Model1
     share::Vector{Float64}
 end
 
-function bet_this(request, model::Model1, round_digits = 6)
+
+
+"""
+    valid_request(r, m::Model[...])
+
+Check if the `r`equest has the correct format. Besides the request `r` this
+function also takes the relevant model as input, so it can check for the necessary 
+fields depending on the model version.
+"""
+function valid_request(r, m::Model1)
+    try
+        haskey(r, "bearOdds") &&
+        haskey(r, "stake") &&
+        r["bearOdds"] isa Number &&
+        r["stake"] isa Number
+    catch
+        false
+    end
+end
+
+
+
+"""
+    bet_this(request, m::Model[...], round_digits = 6)
+
+What amount should be bet on what.
+
+# Arguments
+* `request` - A `Dict`ionary that contains the correct fields.
+* `m` - Model that is applied to the request (`Model1`, `Model2`...)
+* `round_digits` - `Integer` containing the number of digits the resulting amount shall
+    be rounded to, to make "don't-bet"s possible.
+"""
+function bet_this(request, m::Model1, round_digits = 6)
+    valid_request(request, m) && return Dict("on" => "bull", "bet" => "0.0")
     _bob_ = "bull"
     amount = 0
-    for (i, bear_odds) ∈ enumerate(model.bear_odds)
-        if request["bear_odds"] ∈ bear_odds 
-            _bob_ = bob(model.share[i])
-            amount = round(request["stake"] * abs(model.share[i]), digits = round_digits)
+    for (i, bear_odds) ∈ enumerate(m.bear_odds)
+        if request["bearOdds"] ∈ bear_odds 
+            _bob_ = bob(m.share[i])
+            amount = round(request["stake"] * abs(m.share[i]), digits = round_digits)
             return Dict("on" => _bob_, "bet" => string(amount))
         end
     end
